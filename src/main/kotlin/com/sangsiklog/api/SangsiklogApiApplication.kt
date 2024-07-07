03c4938517d42f62f6a14a2135962aa9718b4039
USER <user@AL02490474.local>
2024-07-08 00:35:16 +0900

    프로젝트 세팅

---
+++ src/main/kotlin/com/sangsiklog/api/SangsiklogApiApplication.kt
@@ -0,0 +1,11 @@
+package com.sangsiklog.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SangsiklogApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SangsiklogApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/sangsiklog/api/SangsiklogApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sangsiklog.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SangsiklogApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
