3b2dd79f59276baae721107ce1fe3b22db01a507
SimHongSub <oxhe2038@naver.com>
2024-07-13 02:35:10 +0900

    feat: 패키지 구조 변경 및 예시 소스 추가
    패키지 구조 변경 및 예시 소스 추가
    
    BREAKING CHANGE: https://github.com/SangsikLog/sangsiklog-api/issues/12

--- components/knowledge/src/main/kotlin/com/sangsiklog/domain/TestDomain.kt
+++ components/knowledge/src/main/kotlin/com/sangsiklog/domain/TestDomain.kt
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.SequenceGenerator
 import java.time.LocalDateTime
 
 @Entity
+@SequenceGenerator(name = "TEST_SEQ_GENERATOR", sequenceName = "TEST_SEQ", initialValue = 1, allocationSize = 50)
 class TestDomain(
     id: Long,
     createdAt: LocalDateTime
