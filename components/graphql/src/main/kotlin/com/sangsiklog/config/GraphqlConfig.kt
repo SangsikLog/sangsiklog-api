bb05d78f06bd2e5939b73ead38095d4b777969aa
SimHongSub <oxhe2038@naver.com>
2024-08-12 23:08:12 +0900

    feat: Authorization directive 구현
    Authorization directive 구현
    
    BREAKING CHANGE: https://github.com/SangsikLog/sangsiklog-api/issues/50

--- components/graphql/src/main/kotlin/com/sangsiklog/config/GraphqlConfig.kt
+++ components/graphql/src/main/kotlin/com/sangsiklog/config/GraphqlConfig.kt
@@ -1,6 +1,7 @@
 package com.sangsiklog.config
 
 import com.sangsiklog.directive.AuthenticationDirective
+import com.sangsiklog.directive.AuthorizationDirective
 import graphql.scalars.ExtendedScalars
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,12 +11,13 @@ import org.springframework.graphql.execution.RuntimeWiringConfigurer
 class GraphqlConfig {
 
     @Bean
-    fun runtimeWiringConfigurer(authenticationDirective: AuthenticationDirective, ): RuntimeWiringConfigurer {
+    fun runtimeWiringConfigurer(authenticationDirective: AuthenticationDirective, authorizationDirective: AuthorizationDirective): RuntimeWiringConfigurer {
         return RuntimeWiringConfigurer { wiring ->
             wiring.scalar(ExtendedScalars.Date)
                 .scalar(ExtendedScalars.DateTime)
                 .scalar(ExtendedScalars.GraphQLLong)
                 .directive("authenticate", authenticationDirective)
+                .directive("authorize", authorizationDirective)
         }
     }
 }
