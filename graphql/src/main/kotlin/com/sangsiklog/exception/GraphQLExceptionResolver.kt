94a908c15dc996b1a1377aa53cc64ab9f9c36552
SimHongSub <oxhe2038@naver.com>
2024-09-01 18:29:17 +0900

    feat: graphql 컴포넌트 grpc 예외처리 추가
    graphql 컴포넌트 grpc 예외처리 추가
    
    BREAKING CHANGE: https://github.com/SangsikLog/sangsiklog-api/issues/80

--- graphql/src/main/kotlin/com/sangsiklog/exception/GraphQLExceptionResolver.kt
+++ graphql/src/main/kotlin/com/sangsiklog/exception/GraphQLExceptionResolver.kt
@@ -1,31 +1,55 @@
 package com.sangsiklog.exception
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import graphql.GraphQLError
 import graphql.GraphqlErrorBuilder
 import graphql.schema.DataFetchingEnvironment
+import io.grpc.StatusException
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
 import org.springframework.graphql.execution.ErrorType
 import org.springframework.stereotype.Component
 
 @Component
 class GraphQLExceptionResolver: DataFetcherExceptionResolverAdapter() {
-    override fun resolveToSingleError(ex: Throwable, env: DataFetchingEnvironment): GraphQLError? {
-        if (ex is UnauthorizedException) {
-            return GraphqlErrorBuilder.newError()
-                .errorType(ErrorType.UNAUTHORIZED)
-                .message(ex.message)
-                .path(env.executionStepInfo.path)
-                .location(env.field.sourceLocation)
-                .build()
-        } else if (ex is HttpServiceException) {
-            return GraphqlErrorBuilder.newError()
-                .errorType(ErrorType.BAD_REQUEST)
-                .message(ex.message)
-                .path(env.executionStepInfo.path)
-                .location(env.field.sourceLocation)
-                .build()
-        }
+    private val objectMapper = ObjectMapper()
 
-        return super.resolveToSingleError(ex, env)
+    override fun resolveToSingleError(ex: Throwable, env: DataFetchingEnvironment): GraphQLError? {
+        when (ex) {
+            is UnauthorizedException -> {
+                return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.UNAUTHORIZED)
+                    .message(ex.message)
+                    .path(env.executionStepInfo.path)
+                    .location(env.field.sourceLocation)
+                    .build()
+            }
+            is HttpServiceException -> {
+                return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(ex.message)
+                    .path(env.executionStepInfo.path)
+                    .location(env.field.sourceLocation)
+                    .build()
+            }
+            is StatusException -> {
+                val parts = ex.message?.split(":", limit = 2)
+                val detailMessage = if (parts?.size == 2) {
+                    parts[1].trim()
+                } else {
+                    ex.message
+                }
+
+                return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(detailMessage)
+                    .path(env.executionStepInfo.path)
+                    .location(env.field.sourceLocation)
+                    .build()
+            }
+            else -> {
+                return super.resolveToSingleError(ex, env)
+            }
+        }
     }
 }
