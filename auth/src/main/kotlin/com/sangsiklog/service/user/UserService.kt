1510c95b93a14c337c2d3ca58fe7ed1867c51832
SimHongSub <oxhe2038@naver.com>
2024-09-05 20:52:50 +0900

    feat: 회원가입시 이메일 인증여부 확인하도록 수정
    회원가입시 이메일 인증여부 확인하도록 수정
    
    BREAKING CHANGE: https://github.com/SangsikLog/sangsiklog-api/issues/88

--- auth/src/main/kotlin/com/sangsiklog/service/user/UserService.kt
+++ auth/src/main/kotlin/com/sangsiklog/service/user/UserService.kt
@@ -6,6 +6,7 @@ import com.sangsiklog.domain.user.UserLoginHistory
 import com.sangsiklog.exception.user.UserServiceException
 import com.sangsiklog.repository.user.UserLoginHistoryRepository
 import com.sangsiklog.repository.user.UserRepository
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -17,7 +18,8 @@ import java.time.LocalDateTime
 class UserService(
     private val userRepository: UserRepository,
     private val userLoginHistoryRepository: UserLoginHistoryRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val redisTemplate: RedisTemplate<String, Any>
 ) {
     fun getUserById(userId: Long): User {
         return userRepository.findById(userId)
@@ -31,6 +33,10 @@ class UserService(
 
     @Transactional
     fun createUser(name: String, email: String, password: String): User {
+        if (!isEmailVerified(email)) {
+            throw UserServiceException(HttpStatus.BAD_REQUEST, ErrorType.EMAIL_NOT_VERIFIED)
+        }
+
         val existsUser = userRepository.findByEmail(email).orElse(null)
         if (existsUser != null) {
             throw UserServiceException(HttpStatus.BAD_REQUEST, ErrorType.EMAIL_ALREADY_EXISTS)
@@ -81,5 +87,9 @@ class UserService(
         userLoginHistoryRepository.save(userLoginHistory)
 
         return userLoginHistory
+    }
+
+    private fun isEmailVerified(email: String): Boolean {
+        return redisTemplate.opsForValue().get("verified:$email") as Boolean
     }
 }
--- core/src/main/kotlin/com/sangsiklog/core/api/exception/ErrorType.kt
+++ core/src/main/kotlin/com/sangsiklog/core/api/exception/ErrorType.kt
@@ -8,6 +8,7 @@ enum class ErrorType(
     NOT_FOUND_USER(1000, "사용자를 찾을 수 없습니다."),
     INVALID_OLD_PASSWORD(1001, "기존 비밀번호가 잘못되었습니다."),
     EMAIL_ALREADY_EXISTS(1002, "이미 존재하는 이메일 입니다."),
+    EMAIL_NOT_VERIFIED(1003, "인증되지 않은 이메일 입니다."),
 
     // 2000~2999 AuthServiceException
     INVALID_CREDENTIALS(2000, "유효하지 않은 자격증명"),
