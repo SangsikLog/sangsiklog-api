beb4446d986b387253b1650e62fe3888b2fa1edb
SimHongSub <oxhe2038@naver.com>
2024-09-22 15:28:58 +0900

    feat: 카테고리 도메인 추가
    카테고리 도메인 추가
    
    BREAKING CHANGE: https://github.com/SangsikLog/sangsiklog-api/issues/97

---
+++ content/src/main/kotlin/com/sangsiklog/domain/category/Category.kt
@@ -0,0 +1,37 @@
+package com.sangsiklog.domain.category
+
+import com.sangsiklog.domain.base.BaseEntity
+import com.sangsiklog.domain.knowledge.Knowledge
+import jakarta.persistence.*
+
+@Entity
+@SequenceGenerator(
+    name = "CATEGORY_SEQ_GENERATOR",
+    sequenceName = "CATEGORY_SEQ",
+    initialValue = 1,
+    allocationSize = 50
+)
+@Table(name = "category")
+class Category(
+    name: String
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CATEGORY_SEQ_GENERATOR")
+    @Column(name = "category_id")
+    val id: Long? = null
+
+    @Column(name = "title", nullable = false)
+    var name: String = name
+        protected set
+
+    @OneToMany(mappedBy = "category")
+    var knowledgeList: MutableSet<Knowledge> = HashSet()
+
+    companion object {
+        fun create(name: String): Category {
+            return Category(
+                name = name
+            )
+        }
+    }
+}
---
+++ content/src/main/kotlin/com/sangsiklog/repository/category/CategoryRepository.kt
@@ -0,0 +1,9 @@
+package com.sangsiklog.repository.category
+
+import com.sangsiklog.domain.category.Category
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CategoryRepository: JpaRepository<Category, Long> {
+}
